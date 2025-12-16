dd97cd6892ba6a4ce309d4d00f63f399b5e10823
Lucas_Novello <lucasnovello98@hotmail.com>
2025-12-16 16:34:57 -0300

    Proyecto base Spring Boot con endpoint ping

---
+++ src/main/java/com/lucas/backend_server/PingController.java
@@ -0,0 +1,17 @@
+package com.lucas.backend_server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+
+
+
+
+}
